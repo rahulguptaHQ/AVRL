8456e9d9ab3ab9e8a21f5d64252b5a2b2ea3276b
rahulguptaHQ <79980367+rahulguptaHQ@users.noreply.github.com>
2021-09-18 12:36:05 +0530

    insert file

---
+++ insert.js
@@ -0,0 +1,12 @@
+function myFunction(a, b) {
+  return a * b;
+}
+
+
+function myFunction1() {
+document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function notifyRed() {
+    alert("OnClick");
+}
